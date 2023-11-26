e653bb355844c334fccb93e93072199b86e1bd8a
Cawe541 <cawe.wewe04@gmail.com>
2023-11-26 14:58:30 -0300

    feat(app): add cowboy server to supervision

--- lib/rest_api_pure_elixir/application.ex
+++ lib/rest_api_pure_elixir/application.ex
@@ -8,8 +8,7 @@ defmodule RestApiPureElixir.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: RestApiPureElixir.Worker.start_link(arg)
-      # {RestApiPureElixir.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: RestApiPureElixir.Router, options: [port: 8080]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
